1d25e4a4041a94fbb5131546585210d153cdeaf2
declan-casey <dpcaseyboy99@gmail.com>
2021-02-26 11:33:10 -0500

    DONE

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import EditMenu from "./EditMenu"
+import { useHistory, useParams } from 'react-router-dom'
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, getColors}) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+
 
   const editColor = color => {
     setEditing(true);
@@ -17,14 +21,47 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors(
+          colors.map((item) => {
+            if(item.id === res.data.id){
+              return res.data
+            } else {
+              return item
+            }
+          })
+        )
+        setEditing(false)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   };
 
-  const deleteColor = color => {
+
+  const deleteColor = (colorToEdit) => {
+    axiosWithAuth()
+      .delete(`/colors/${colorToEdit.id}`)
+      .then(res => {
+        getColors()
+        
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.href =  '/';
   };
 
   return (
     <div className="colors-wrap">
+      <button onClick = {logout}>Logout</button>
       <p>colors</p>
       <ul>
         {colors.map(color => (
@@ -36,7 +73,7 @@ const ColorList = ({ colors, updateColors }) => {
                   }
                 }>
                   x
-              </span>{" "}
+              </span>{""}
               {color.color}
             </span>
             <div
@@ -53,7 +90,6 @@ const ColorList = ({ colors, updateColors }) => {
 };
 
 export default ColorList;
-
 //Task List:
 //1. Complete the saveEdit functions by making a put request for saving colors. (Think about where will you get the id from...)
 //2. Complete the deleteColor functions by making a delete request for deleting colors.
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const Login = () => {
+const initialFormValues = {
+  username:'',
+  password:''
+}
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-
+  const [formValues, setFormValues] = useState(initialFormValues);
   useEffect(()=>{
+
+    
     axios
       .delete(`http://localhost:5000/api/colors/1`, {
         headers:{
@@ -25,12 +31,47 @@ const Login = () => {
       })
   });
 
+  const handleChange = e => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', formValues)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload);
+      props.history.push('/protected')
+      console.log("success", res.data)
+    })
+    .catch( err => {
+      console.log("Heres where you went wrong", err)
+    })
+  }
   return (
     <>
       <h1>
         Welcome to the Bubble App!
-        <p>Build a login page here</p>
       </h1>
+      <form onSubmit = {login}>
+        <label> Username:
+          <input
+            type = "text"
+            name = "username" 
+            values = {formValues.username}
+            onChange = {handleChange}/>
+        </label>
+        <label>Password:
+          <input
+            type = "password"
+            name = "password"
+            values = {formValues.password}
+            onChange = {handleChange}/>
+        </label>
+        <button>Submit</button>
+      </form>
     </>
   );
 };
